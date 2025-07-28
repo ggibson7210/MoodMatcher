cb844cbac1e30457f8d83a8439d0a592c44d5310
Gwyneth <ggibson7210@gmail.com>
2025-07-28 10:34:28 -0400

    Initial Commit

---
+++ MoodMatcher/MoodMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMatcherApp.swift
+//  MoodMatcher
+//
+//  Created by Scholar on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
