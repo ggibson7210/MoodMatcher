07b1748358ea0a51d1bc775ebf466b13bcf7789e
Gwyneth <ggibson7210@gmail.com>
2025-07-30 14:56:32 -0400

    made 4 activity pages with color and functionality

---
+++ MoodMatcher/TheBluesActivity.swift
@@ -0,0 +1,18 @@
+//
+//  TheBluesActivity.swift
+//  MoodMatcher
+//
+//  Created by Scholar on 7/30/25.
+//
+
+import SwiftUI
+
+struct TheBluesActivity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TheBluesActivity()
+}
